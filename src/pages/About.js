7f926331aebe23a6f5dd7f069047ceaf6de56d40
Moshiko-Start <moshiko14691@gmail.com>
2021-12-05 14:30:20 +0200

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<section className="section about-section">
+			<h1 className="section-title">about us</h1>
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam
+				omnis optio dolorum hic voluptas itaque quia, recusandae tempore
+				distinctio necessitatibus at natus nesciunt, inventore
+				consequatur quam commodi autem assumenda. Architecto cum sunt
+				autem commodi aliquam maiores est voluptate voluptas. Tempora.
+			</p>
+		</section>
+	);
+};
+
+export default About;
